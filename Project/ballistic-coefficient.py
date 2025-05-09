026be68fe0ea8f0e3651859965ee47f507bf04a4
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2025-05-09 00:08:03 -0700

    Back test and Ballistic Coefficients

--- Project/ballistic-coefficient.py
+++ Project/ballistic-coefficient.py
@@ -2,15 +2,17 @@ import sim
 import matplotlib.pyplot as plt
 import numpy as np
 
+file_path = 'Ballistic-Coefficient-Charts/'
+
 time_step = 0.1
 time_max = 5000
 entry_altitude = 125000
 entry_flight_path_angle = -15
 entry_velocity = 6000
 
-area = 10
-masses = [0.001,0.01,0.1,1,10,100,1000,4444,10000,100000,1000000]  # kg
-ballistic_coefficients = [mass / area for mass in masses]
+area = 1
+ballistic_coefficients = [0.01,0.1,1,10,100,220,1000,4444,10000,100000,1000000,10000000]  # kg/m^2
+masses = [ballistic_coefficient * area for ballistic_coefficient in ballistic_coefficients]
 terminal_velocities = []
 
 for mass in masses:
@@ -24,9 +26,16 @@ for mass in masses:
         entry_velocity=entry_velocity,
         print_debug=False,
     )
-    sim.plot(sim_data, filename=f"Ballistic-Coefficient-{str(mass).replace(".", "")}kgm2")
+    sim.plot(sim_data, filename=f"{file_path}Ballistic-Coefficient-{str(mass).replace(".", "")}kgm2")
     terminal_velocities.append(sim_data['final_values']['velocity'])
     print(f"Mass: {mass} kg, Terminal Velocity: {sim_data['final_values']['velocity']:.2f} m/s")
+
+print("Ballistic Coefficient,Terminal Velocity")
+for i, mass in enumerate(masses):
+    print(f"{ballistic_coefficients[i]},{terminal_velocities[i]:.2f}")
+
+annotation_indexes = [5,7]
+annoation_labels = ['Curiosity','Starship']
 
 # Plot in plt using log scale
 plt.figure(figsize=(10, 6))
@@ -37,9 +46,9 @@ plt.xlabel('Ballistic Coefficient (kg/m^2)')
 plt.ylabel('Terminal Velocity (m/s)')
 plt.title('Ballistic Coefficient vs Terminal Velocity')
 plt.grid(True)
-plt.annotate('Starship', xy=(masses[7], terminal_velocities[7]), xytext=(masses[7]*1.5, terminal_velocities[7]*1.5))
-plt.savefig('ballistic_coefficient_vs_terminal_velocity_log.png')
-plt.show()
+for i, label in enumerate(annoation_labels):
+    plt.annotate(label, xy=(ballistic_coefficients[annotation_indexes[i]], terminal_velocities[annotation_indexes[i]]), xytext=(ballistic_coefficients[annotation_indexes[i]], terminal_velocities[annotation_indexes[i]]*0.75))
+plt.savefig(f'{file_path}ballistic_coefficient_vs_terminal_velocity_log.png')
 plt.close()
 
 # now without log scale
@@ -49,7 +58,7 @@ plt.xlabel('Ballistic Coefficient (kg/m^2)')
 plt.ylabel('Terminal Velocity (m/s)')
 plt.title('Ballistic Coefficient vs Terminal Velocity')
 plt.grid(True)
-plt.annotate('Starship', xy=(masses[7], terminal_velocities[7]), xytext=(masses[7]*1.5, terminal_velocities[7]*1.5))
-plt.savefig('ballistic_coefficient_vs_terminal_velocity_linear.png')
-plt.show()
+for i, label in enumerate(annoation_labels):
+    plt.annotate(label, xy=(ballistic_coefficients[annotation_indexes[i]], terminal_velocities[annotation_indexes[i]]), xytext=(ballistic_coefficients[annotation_indexes[i]], terminal_velocities[annotation_indexes[i]]*0.75))
+plt.savefig(f'{file_path}ballistic_coefficient_vs_terminal_velocity_linear.png')
 plt.close()

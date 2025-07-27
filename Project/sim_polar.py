314a1dcfbe01d459952ea5de3425ccd341a94c76
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2025-07-27 14:47:09 -0700

    Plots work! Beautiful refactor!

--- Project/sim_polar.py
+++ Project/sim_polar.py
@@ -20,11 +20,26 @@ from dataclasses import dataclass
 # - Sideslip angle (horizontal plane angle, where angle of attack is vertical plane angle)
 
 @dataclass
-class SimStep:
-    """Single simulation timestep data - clean and compact."""
-    t: float; alt: float; r_dist: float; ang_dist: float; a_rad: float; a_ang: float
-    a_grav: float; a_drag: float; a_lift: float; v_net: float; v_rad: float; v_ang: float
-    fpa: float; aoa: float; p_atm: float; t_atm: float; rho_atm: float; sim_time: float
+class SimData:
+    """All simulation data as lists - ready for plotting."""
+    t: list = None; alt: list = None; r_dist: list = None; ang_dist: list = None
+    a_rad: list = None; a_ang: list = None; a_grav: list = None; a_drag: list = None; a_lift: list = None
+    v_net: list = None; v_rad: list = None; v_ang: list = None; fpa: list = None; aoa: list = None
+    p_atm: list = None; t_atm: list = None; rho_atm: list = None; sim_time: list = None
+
+    def __post_init__(self):
+        for field in self.__dataclass_fields__:
+            if getattr(self, field) is None:
+                setattr(self, field, [])
+    
+    def add(self, *values):
+        """Add timestep data - just pass 18 values in order."""
+        for field, value in zip(self.__dataclass_fields__, values):
+            getattr(self, field).append(value)
+    
+    def __getitem__(self, i):
+        """Get timestep i as dict."""
+        return {f: getattr(self, f)[i] for f in self.__dataclass_fields__}
 
 def simulate(mass, area, entry_altitude, entry_flight_path_angle, entry_velocity, aoa_function=None, time_step=0.1, time_max=1000, verbose=False):
     """Simulate Mars entry trajectory.
@@ -62,7 +77,7 @@ def simulate(mass, area, entry_altitude, entry_flight_path_angle, entry_velocity
     flight_path_angle = entry_flight_path_angle
     
     # Data collection
-    data = []
+    data = SimData()
     
     t = 0
     while t < time_max and altitude > 0:
@@ -94,15 +109,12 @@ def simulate(mass, area, entry_altitude, entry_flight_path_angle, entry_velocity
         angular_distance_m += vel_ang_m * time_step
         altitude = radial_distance - utils.MARS_RADIUS
         
-        # Save data in one line
-        data.append(SimStep(t, altitude, radial_distance, angular_distance_m, a_rad, a_ang, a_grav, a_drag, a_lift, vel_net_m, vel_rad_m, vel_ang_m, flight_path_angle, aoa, atm_pressure, atm_temperature, atm_density, time.time() - simulation_start_time))
-
-        if verbose:
-            print(f"{data[-1]}\n")
+        data.add(t, altitude, radial_distance, angular_distance_m, a_rad, a_ang, a_grav, a_drag, a_lift, vel_net_m, vel_rad_m, vel_ang_m, flight_path_angle, aoa, atm_pressure, atm_temperature, atm_density, time.time() - simulation_start_time)
+        if verbose: print(f"{data[-1]}\n")
 
         t += time_step
 
-    impact_values = data[-1]
+    impact_values = data[-1]  # Last timestep as dict
     parameters = {'mass': mass, 'area': area, 'entry_altitude': entry_altitude, 'entry_flight_path_angle': entry_flight_path_angle, 'entry_velocity': entry_velocity, 'time_step': time_step, 'time_max': time_max}
     
     return data, impact_values, parameters
@@ -130,41 +142,85 @@ def plot(data, parameters, impact_values, title="Mars Entry Simulation", file_na
     # Comparisions is a list of tuples (velocity, altitude, label), MAKE SURE ITS A LIST, NOT JUST A TUPLE, USE THE SQUARE BRACKETS
     if comparisons is None:
         comparisons = []
+        
+    plt.figure(figsize=(19.20, 10.80), dpi=100)
+    plt.suptitle(title, fontsize=16) # Supertitle
+    plt.gcf().text(0.01, 0.965, f"Christopher Kalitin 2025", fontsize=12)
+    plt.axis('off')
+    
+    sub_plot((3,3,1), "Altitude vs Time", "Time (s)", "Altitude (m)", data.v_net, data.alt, "Simulation")
+    
+    plt.subplots_adjust(left=0.055, right=0.98, top=0.925, bottom=0.042, hspace=0.29, wspace=0.31)
 
-def sub_plot(position, title, x_label, y_label, series_x, series_y_lists, series_labels, comparisons=None, comparison_series_x_key=None, comparison_series_y_keys=None, comparison_series_y_label_keys=None):
-    """Create a subplot for the given data.
+    plt.savefig(file_name)
+    if show: plt.show()
+    plt.close()
 
-    Args:
-        title (_type_): _description_
-        x_label (_type_): _description_
-        y_label (_type_): _description_
-        series_x (_type_): _description_
-        series_y_lists (_type_): _description_
-        series_labels (_type_): _description_
-        comparisons (_type_): _description_
-        comparison_series_x_titlecomparison_series_y_titles (_type_): _description_
+def sub_plot(position, title, x_label, y_label, x_data, y_data_list, series_labels, comparisons=[], comparison_x_key=None, comparison_y_keys=None, comparison_y_label_keys=None):
     """
+    Creates a subplot and plots multiple data series, with optional comparison series.
+        position (tuple): A tuple (nrows, ncols, index) specifying the subplot position.
+        title (str): The title of the subplot.
+        x_label (str): The label for the x-axis.
+        y_label (str): The label for the y-axis.
+        x_data (array-like): The data for the x-axis, shared by all main series.
+        y_data_list (list of array-like): List of y-axis data arrays, one for each main series.
+        series_labels (list of str): List of labels for each main series.
+        comparisons (list of dict, optional): List of comparison data dictionaries. Each dictionary should contain keys for x and y data and label.
+        comparison_x_key (list of str, optional): List of keys to extract x-axis data from each comparison dictionary.
+        comparison_y_keys (list of str, optional): List of keys to extract y-axis data from each comparison dictionary.
+        comparison_y_label_keys (list of str, optional): List of keys to extract label for each comparison series.
+    Returns:
+        None: The function creates a subplot and plots the provided data.
+    Notes:
+        - Adds a legend if more than one series is plotted.
+        - Comparison series are plotted with dashed lines.
+        - array-like means any structure that can be converted to a numpy array, such as lists or numpy arrays.
+    """
+
+    # If these are lists of strs and not lists of lists of strs, convert
+    if (y_data_list is not None and type(y_data_list[0]) != list): y_data_list = [y_data_list]
+    if (series_labels is not None and type(series_labels[0]) != list): series_labels = [series_labels]
+    if (comparison_y_keys is not None and type(comparison_y_keys[0]) != list): comparison_y_keys = [comparison_y_keys]
+    if (comparison_y_label_keys is not None and type(comparison_y_label_keys[0]) != list): comparison_y_label_keys = [comparison_y_label_keys]
+
+    zorder = 999
+    
+    plt.subplot(position[0], position[1], position[2])
+
+    for y_data, label in zip(y_data_list, series_labels):
+        plt.plot(x_data, y_data, label=label, zorder=zorder)
+        zorder -= 1
+        
+    for comparison in comparisons:
+        for x_key, y_key, y_label_key in zip(comparison_x_key, comparison_y_keys, comparison_y_label_keys):
+            if x_key in comparison and y_key in comparison and y_label_key in comparison:
+                plt.plot(comparison[x_key], comparison[y_key], '--', label=comparison[y_label_key])
+
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    if len(y_data_list) + len(comparisons) > 1: plt.legend()
+    plt.grid(True)
+    
+def text_sub_plot(position, parameters, impact_values):
+    """
+    Creates a subplot and plots multiple data series with text annotations.
+    """
+    
+    if type(y_data_list) != list: y_data_list = [y_data_list]
+    if type(series_labels) != list: series_labels = [series_labels]
+    if type(comparison_y_keys) != list: comparison_y_keys = [comparison_y_keys]
+    if type(comparison_y_label_keys) != list: comparison_y_label_keys = [comparison_y_label_keys]
     
     z_order = 999
     
     plt.subplot(position[0], position[1], position[2])
-    
-    for series_y_list, label in zip(series_y_lists, series_labels):
-        plt.plot(series_x, series_y_list, label=label, z_order=z_order)
-        z_order -= 1
 
-    if comparisons is not None:
-        for comparison, x_key, y_key, y_label_key in zip(comparisons, comparison_series_x_key, comparison_series_y_keys, comparison_series_y_label_keys):
-            plt.plot(comparisons[x_key], comparisons[y_key], '--', label=comparison[y_label_key])
+    for y_data, label in zip(y_data_list, series_labels):
+        pass    
 
-    plt.title(plt.title)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    if len(series_y_lists) + len(comparisons) > 1:
-        plt.legend()
-    plt.grid(True)
-    
-simulate(
+data, impact_values, parameters = simulate(
     mass=1000, 
     area=10, 
     entry_altitude=100000, 
@@ -172,6 +228,8 @@ simulate(
     entry_velocity=5000, 
     aoa_function=5,
     time_step=1, 
-    time_max=10, 
+    time_max=1000, 
     verbose=True
 )
+
+plot(data, parameters, impact_values, show=True, file_name="test.png")

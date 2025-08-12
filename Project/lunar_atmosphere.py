eebd3e706e655bc2356a6d2e5ef23763df557086
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2025-08-11 23:27:56 -0700

    Final Lunar Commit

--- Project/lunar_atmosphere.py
+++ Project/lunar_atmosphere.py
@@ -1,6 +1,8 @@
 import sim_polar
 import utils_data
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 BOLTZMANN_CONSTANT = 1.380649e-23 # J/K
 ATMOSPHERIC_TEMPERATURE = 250 # K
@@ -28,9 +30,6 @@ def set_body_pressure(sim_parameters: utils_data.SimParameters, surface_pressure
     
     def plot():
         # Plot Atmosphere using matplotlib
-        import matplotlib.pyplot as plt
-        import matplotlib.ticker as ticker
-
         plt.figure()
         plt.semilogy(sim_parameters.pressure_data, sim_parameters.pressure_alt_data)
         plt.title('Lunar Atmosphere Pressure vs Altitude (Example 1e5 Pa surface pressure)')
@@ -61,7 +60,7 @@ def set_aerodynamic_parameters(sim_parameters: utils_data.SimParameters, vehicle
         # Using the same paper, assuming 30 degree constant angle of attack and mach 4ish because I'm not putting that much effort into this
         # https://aviation.stackexchange.com/questions/35305/what-was-the-space-shuttles-glide-ratio
         sim_parameters.cd_drag_coeff_data = np.array([0.42, 0.42])
-        sim_parameters.cd_vel_data = np.array([-100000, 100000])  # m/s
+        sim_parameters.cd_vel_data = np.array([100000, -100000])  # m/s
         
         # Lift to drag ratio data, lift to drag ratio vs. AoA (degrees)
         # This is actually dependent on velocity too, which I missed in my first simulation. I'll go with Mach=2 line
@@ -74,7 +73,7 @@ def set_aerodynamic_parameters(sim_parameters: utils_data.SimParameters, vehicle
         ])
         
     # All original values for a Mars blunt body (Used Pheonix lander data)
-    elif vehicle == 'blunt_body':
+    elif vehicle == 'blunt_body' or vehicle == 'starship':
         # Coefficient of drag data, drag coefficient vs. velocity (m/s)
         sim_parameters.cd_drag_coeff_data = np.array([
             1.73, 1.72, 1.71, 1.71, 1.72, 1.73, 1.73, 1.73, 1.73, 1.73,
@@ -101,25 +100,98 @@ def set_aerodynamic_parameters(sim_parameters: utils_data.SimParameters, vehicle
             -90,-16,-11,-6,-4,-2,0,2,4,6,11,16,90
     ])
 
-sim_parameters = utils_data.SimParameters(body='Lunar')
-set_body_parameters(sim_parameters)
-set_body_pressure(sim_parameters, surface_pressure=1e-5)
-set_aerodynamic_parameters(sim_parameters, vehicle='blunt_body')
+def test():
+    sim_parameters = utils_data.SimParameters(body='Lunar')
+    set_body_parameters(sim_parameters)
+    set_body_pressure(sim_parameters, surface_pressure=1e-5)
+    set_aerodynamic_parameters(sim_parameters, vehicle='blunt_body')
 
-data, params = sim_polar.simulate(
-    sim_parameters=sim_parameters,
-    mass=1000,
-    area=10,
-    entry_altitude=200000,
-    entry_flight_path_angle=0,
-    entry_velocity=1600,
-    aoa_function=20,
-    time_step=0.1,
-    time_max=10000,
-    verbose=False,
-)
+    data, params = sim_polar.simulate(
+        sim_parameters=sim_parameters,
+        mass=1000,
+        area=10,
+        entry_altitude=200000,
+        entry_flight_path_angle=0,
+        entry_velocity=1600,
+        aoa_function=20,
+        time_step=0.1,
+        time_max=10000,
+        verbose=False,
+    )
 
-# 1591 m/s is orbital velocity at 200 km.
-# 1600 m/s is 243x200 km, if starting at 200 km.
+    # 1591 m/s is orbital velocity at 200 km.
+    # 1600 m/s is 243x200 km, if starting at 200 km.
 
-sim_polar.plot(sim_parameters, data, params, title="Lunar Entry Simulation", file_name="lunar_test.png", show=True)
+    sim_polar.plot(sim_parameters, data, params, title="Lunar Entry Simulation", file_name="lunar_test.png", show=True)
+    
+def sim_atmospheres(surface_atmospheric_pressures, file_path="Lunar Atmosphere/"):
+    vehicles = ['shuttle', 'blunt_body', 'starship']
+    results = {vehicle: [] for vehicle in vehicles}
+    params = None
+    for vehicle in vehicles:
+        sim_parameters = utils_data.SimParameters(body='Lunar')
+        set_body_parameters(sim_parameters)
+        set_aerodynamic_parameters(sim_parameters, vehicle=vehicle)
+
+        for atm_p in surface_atmospheric_pressures:
+            print(f"=== Running {vehicle} with surface pressure {atm_p} Pa ===")
+            set_body_pressure(sim_parameters, atm_p)
+
+            aoa = 0 if vehicle == 'blunt_body' else 10 # Constnat 10 AoA for shuttle
+
+            mass = 4000 if vehicle == 'starship' else 1000
+
+            data, params = sim_polar.simulate(
+                sim_parameters=sim_parameters,
+                mass=mass,
+                area=10,
+                entry_altitude=200000,
+                entry_flight_path_angle=-5,
+                entry_velocity=1600,
+                aoa_function=aoa,
+                time_step=0.1,
+                time_max=10000,
+                verbose=False,
+            )
+
+            sim_polar.plot(
+                sim_parameters,
+                data, params,
+                title=f"{vehicle} Entry Simulation at {atm_p} Pa",
+                file_name=f"{file_path}/lunar_{vehicle}_atm_{atm_p}.png",
+                show=False
+            )
+            results[vehicle].append(data.v_net[-1])  # Store terminal velocity
+    
+    # combined log–log plot
+    plt.figure(figsize=(9.6, 5.4), dpi=200)
+    for vehicle, label in zip(results.keys(), ['Shuttle 100 kg/m² 10° AoA', 'Blunt Body 100 kg/m² 0° AoA', 'Starship 400 kg/m² 30° AoA']):
+        plt.plot(surface_atmospheric_pressures, results[vehicle], marker='.', label=label)
+    plt.xscale('linear')
+    plt.yscale('linear')
+    plt.xlabel('Surface Atmospheric Pressure (Pa)')
+    plt.ylabel('Impact Velocity (m/s)')
+    plt.title('Lunar Entry Vehicle Impact Velocity vs Lunar Surface Atmospheric Pressure')
+    plt.grid(True, which='both', ls='--', alpha=0.5)
+
+    plt.legend(title='Vehicle')
+
+    textbox_text = f"""
+        Christopher Kalitin 2025
+        Entry Altitude: {round(params['entry_altitude']/1000)} km
+        Entry Velocity: {round(params['entry_velocity']/1000, 2)} km/s
+        Entry Flight Path Angle: {params['entry_flight_path_angle']}°
+        Time Step: {params['time_step']} s
+        Atm. Scale Height: 32.1 km       
+    """
+
+    plt.text(0.640, 0.745, textbox_text,
+            transform=plt.gca().transAxes, fontsize=10,
+            verticalalignment='top', horizontalalignment='left',
+            bbox=dict(boxstyle="round,pad=0.2", edgecolor='black', facecolor='white'))
+
+    plt.savefig(f"{file_path}impact_velocity_vs_atm_t{params['time_step']}_linear.png")
+    plt.close()
+
+atms = np.array([10, 50, 100, 500, 1000, 2000])  # Pa
+sim_atmospheres(atms, file_path="Lunar Atmosphere/")
